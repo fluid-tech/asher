430bd88a0a9878754b7f01723a8371840d1578be
Rohan Nair <rrnair98@gmail.com>
2020-05-13 12:05:12 +0530

    adding GetFileMap

--- internal/api/emitter.go
+++ internal/api/emitter.go
@@ -2,5 +2,14 @@ package api
 
 // Emits level 1 nodes in the JSON
 type Emitter interface {
-	Emit(key string, value interface{})
+	/**
+	Callback triggered by AsherWalker.
+	value is usually the data stored in a model object
+	*/
+	Emit(value interface{})
+	/**
+	Callback triggered by AsherWalker once it emits all nodes.
+	This contains the path and the contents of the files to write
+	*/
+	GetFileMap() map[string]EmitterFile
 }
