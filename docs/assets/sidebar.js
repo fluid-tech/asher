ab3cbf56e42af4629e833cc70ec6b76465a07603
Gaurav Punjabi <gaurav.bpunjabi@gmail.com>
2020-05-13 18:55:08 +0530

    Adding: Initial setup for documentation

---
+++ docs/assets/sidebar.js
@@ -0,0 +1,39 @@
+const Sidebar = [
+    {
+        title: "Introduction",
+        link: '/introduction'
+    },
+    {
+        title: 'Guide',
+        children: [
+            {
+                title: 'Introduction',
+                link: '/'
+            },
+            {
+                title: 'Customization',
+                link: '/guide/customization'
+            },
+            {
+                title: 'Markdown Features',
+                link: '/guide/markdown-features'
+            },
+            {
+                title: 'Use Vue in Markdown',
+                link: '/guide/use-vue-in-markdown'
+            },
+            {
+                title: 'Internationalization',
+                link: '/guide/internationalization'
+            },
+            {
+                title: 'Plugin',
+                link: '/guide/plugin'
+            },
+            {
+                title: 'Deployment',
+                link: '/guide/deployment'
+            }
+        ]
+    },
+];
