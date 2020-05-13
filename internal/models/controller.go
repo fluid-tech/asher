1a5282c56b6f79504de9d264e8155f60a1e220ef
Rohan Nair <rrnair98@gmail.com>
2020-05-13 12:03:06 +0530

    nesting pattern in controller

--- internal/models/controller.go
+++ internal/models/controller.go
@@ -1,6 +1,7 @@
 package models
 
 type Controller struct {
-	Rest bool `json:"rest"`
-	Mvc  bool `json:"mvc"`
+	Rest    bool    `json:"rest"`
+	Mvc     bool    `json:"mvc"`
+	Pattern Pattern `json:"pattern"`
 }
