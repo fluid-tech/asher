867c8ffc58bf5c5be4718c190107eff1fb2f0279
Rohan Nair <rrnair98@gmail.com>
2020-05-14 18:51:29 +0530

    adding test

---
+++ test/internal/api/codebuilder/php/builder/class_test.go
@@ -0,0 +1,50 @@
+package builder
+
+import (
+	"asher/internal/api/codebuilder/php/builder"
+	"asher/internal/api/codebuilder/php/core"
+	"fmt"
+	"testing"
+)
+const TEST_CLASS string =
+`namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class JobProfile extends Model
+{
+    public $table = "op_job_profiles";
+}
+`
+func TestClassBuilder(t *testing.T)  {
+
+	assignmentSS := &core.SimpleStatement{
+		SimpleStatement: "$this->mutator = mutator",
+	}
+	function := &builder.Function{}
+	function.SetName("hello").SetVisibility("public")
+	function.AddArgument("BaseMutator mutator")
+	tab := core.TabbedUnit(assignmentSS)
+	function.AddStatement(&tab)
+
+	fmt.Println(function.GetFunction().String())
+
+	statement := &core.SimpleStatement{
+		SimpleStatement: `public $table="op_job_profiles"`,
+	}
+	klass := builder.Class{}
+	klass.SetName("JobProfile").SetExtends("Model")
+	member := core.TabbedUnit(statement)
+	klass.AddMembers([]*core.TabbedUnit{
+		&member,
+	}).AddFunction(function.GetFunction())
+
+	fmt.Println(len(klass.GetClass().Members))
+
+	if klass.GetClass().String() != TEST_CLASS{
+		t.Error("expected ", klass.GetClass().String(), " \n\n\n found", TEST_CLASS)
+	}
+
+
+
+}
---
+++ test/internal/impl/laravel/5.8/handler/context/migration_test.go
@@ -0,0 +1,31 @@
+package context
+
+import (
+	"asher/internal/api/codebuilder/php/core"
+	"asher/internal/impl/laravel/5.8/handler/context"
+	"testing"
+)
+
+func Test_MigrationContext(t *testing.T) {
+	migration := context.GetFromRegistry("migration")
+	var classes = []struct {
+		class        *core.Class
+		expectedName string
+	}{
+		{getClass("Hello"), "Hello"},
+		{getClass("World"), "World"},
+	}
+	for _, element := range classes {
+		migration.AddToCtx(element.class.Name, element.class)
+		if migration.GetCtx(element.expectedName).(*context.MigrationInfo).Class.Name != element.expectedName {
+			t.Error("Unexpected data")
+		}
+	}
+}
+
+func getClass(name string) *core.Class {
+	class := core.Class{}
+	class.Name = name
+	class.Tabs = 0
+	return &class
+}
