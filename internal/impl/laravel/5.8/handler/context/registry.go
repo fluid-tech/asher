b9403c7778c5001cda0f1b3f7f46048e1a310785
Rohan Nair <rrnair98@gmail.com>
2020-05-14 18:20:47 +0530

    adding context registry

---
+++ internal/impl/laravel/5.8/handler/context/registry.go
@@ -0,0 +1,15 @@
+package context
+
+var registry = map[string]BaseContext{
+	"migration": NewMigrationContext(),
+	"model" : NewModelContext(),
+}
+
+/*
+Fetches a BaseContext implementation from the registry. This method
+exists only to avoid writes to this map from outside this package
+ */
+func GetFromRegistry(key string) BaseContext {
+	return registry[key]
+}
+
