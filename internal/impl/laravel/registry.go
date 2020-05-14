2df4060c079d807225c706b7353d752cfd087799
Rohan Nair <rrnair98@gmail.com>
2020-05-14 18:20:30 +0530

    adding registry

---
+++ internal/impl/laravel/registry.go
@@ -0,0 +1,18 @@
+package laravel
+
+import (
+	"asher/internal/api"
+	"asher/internal/impl/laravel/5.8/handler"
+)
+
+var handlerRegistry = map[string]api.Handler{
+	"auditCol": handler.NewAuditColHandler(),
+}
+
+/**
+Returns a Handler instance from the registry.
+This method
+exists only to avoid writes to this map from outside this package */
+func GetFromRegistry(key string) api.Handler {
+	return handlerRegistry[key]
+}
