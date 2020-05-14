81f4cc9148fc5b27b14a4f7e6bd2d611b6c70d0f
Rohan Nair <rrnair98@gmail.com>
2020-05-14 18:23:34 +0530

    adding String to TabbedUnit

--- internal/api/codebuilder/php/core/tabbed_unit.go
+++ internal/api/codebuilder/php/core/tabbed_unit.go
@@ -1,11 +1,16 @@
-package php
+package core
 
 import "strings"
 
 type TabbedUnit interface {
-	ToString() string
+	SetNumTabs(tabs int)
+	Id() string // something that uniquely identifies this tabbed unit, in cases of vars it could be the var name
+	String() string
 }
 
-func TabbedString(numTabs uint) string  {
-	return strings.Repeat(" ", int(numTabs << 4))
+func TabbedString(numTabs uint) string {
+	if numTabs > 0 {
+		return strings.Repeat(" ", int(numTabs<<2))
+	}
+	return ""
 }
