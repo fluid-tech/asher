4f66a0a939dcf00f225f7aaa97943c55b197df5d
Rohan Nair <rrnair98@gmail.com>
2020-05-14 18:21:06 +0530

    adding model ctx

--- internal/impl/laravel/5.8/handler/context/model.go
+++ internal/impl/laravel/5.8/handler/context/model.go
@@ -1,2 +1,20 @@
 package context
 
+import "asher/internal/api/codebuilder/php/core"
+
+type Model struct {
+	BaseContext
+	modelMap map[string]*core.Class
+}
+
+func NewModelContext() *Model {
+	return &Model{modelMap: make(map[string]*core.Class)}
+}
+
+func (m *Model) AddToCtx(key string, value interface{}) {
+	m.modelMap[key] = value.(*core.Class)
+}
+
+func (m *Model) GetCtx(key string) interface{} {
+	return m.modelMap[key]
+}
