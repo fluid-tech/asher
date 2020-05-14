6bfd8e23f081058eed355cd1676d130476a8c140
Rohan Nair <rrnair98@gmail.com>
2020-05-14 20:11:46 +0530

    adding var decl

---
+++ internal/api/codebuilder/php/core/var_declaration.go
@@ -0,0 +1,37 @@
+package core
+
+import (
+	"fmt"
+	"strings"
+)
+
+type VarDeclaration struct {
+	TabbedUnit
+	tabs       int
+	Visibility string
+	Identifier string
+}
+
+func GetVarDeclaration(visibility string, id string, rhs string) *VarDeclaration {
+	return &VarDeclaration{
+		tabs:       0,
+		Visibility: visibility,
+		Identifier: id,
+	}
+}
+
+
+func (v *VarDeclaration) SetNumTabs(tabs int) {
+	v.tabs = tabs
+}
+
+func (v *VarDeclaration) Id() string {
+	return v.Identifier
+}
+
+func (v *VarDeclaration) String() string  {
+	var builder strings.Builder
+	fmt.Fprintf(&builder, TabbedString(uint(v.tabs)),
+		v.Visibility, " $", v.Identifier, ";\n")
+	return builder.String()
+}
