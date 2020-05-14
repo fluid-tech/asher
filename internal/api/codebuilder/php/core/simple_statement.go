569d035c4d81204372de3911abbe6c3ba475a405
Rohan Nair <rrnair98@gmail.com>
2020-05-14 18:23:53 +0530

    adding ctor

--- internal/api/codebuilder/php/core/simple_statement.go
+++ internal/api/codebuilder/php/core/simple_statement.go
@@ -1,9 +1,32 @@
 package core
 
-type SimpleStatement struct{
-	tabbedUnit      TabbedUnit
-	simpleStatement string
+import (
+	"fmt"
+	"strings"
+)
+
+type SimpleStatement struct {
+	TabbedUnit
+	numTabs         int
+	SimpleStatement string
 }
 
+func GetSimpleStatement(simpleStatement string) *SimpleStatement {
+	return &SimpleStatement{
+		SimpleStatement: simpleStatement,
+	}
+}
 
+func (stmt *SimpleStatement) SetNumTabs(tabs int) {
+	stmt.numTabs = tabs
+}
 
+func (stmt *SimpleStatement) Id() string {
+	return stmt.SimpleStatement
+}
+
+func (stmt *SimpleStatement) String() string {
+	var builder strings.Builder
+	fmt.Fprint(&builder, TabbedString(uint(stmt.numTabs)), stmt.SimpleStatement, ";\n")
+	return builder.String()
+}
