852a2973f7dfc0a8dcccab75f2f2b5f57919e490
Rohan Nair <rrnair98@gmail.com>
2020-05-15 10:40:48 +0530

    adding file type to emitter

--- internal/api/emitter_file.go
+++ internal/api/emitter_file.go
@@ -1,7 +1,17 @@
 package api
 
-type EmitterFile struct{
-	FileName string
-	Path string
-	Content string
+type EmitterFile struct {
+	FileName string // name of the file
+	Path     string // path to store it in
+	Content  string // content of the file
+	FileType int	// 0 - migration, 1 - model, 2- mutator, 3-transactor
+}
+
+func NewEmitterFile(fileName string, path string, content string, fileType int) *EmitterFile {
+	return &EmitterFile{
+		FileName:fileName,
+		Path:     path,
+		Content:  content,
+		FileType: fileType,
+	}
 }
