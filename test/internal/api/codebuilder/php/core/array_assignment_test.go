3dc6cd635ea757742697d131a8790a68f2c0ce8f
Pranay Patro <pranaypatro11@gmail.com>
2020-05-16 02:49:39 +0530

    Merge Solved

---
+++ test/internal/api/codebuilder/php/core/array_assignment_test.go
@@ -0,0 +1,33 @@
+package core
+
+import (
+	"asher/internal/api/codebuilder/php/core"
+	"asher/test/api"
+	"testing"
+)
+
+const rhsOut = `public $fillable = ["user_id", 
+"password"
+];
+`
+const rhsOut2 = `private $fillable = ["user_id", 
+"password"
+];
+`
+
+const rhsOut3 = ` $fillable = ["user_id", 
+"password"
+];
+`
+
+func TestArrayAssignment(t *testing.T) {
+	rhs := []string{`"user_id"`, `"password"`}
+	var table = []*api.GeneralTest{
+		api.NewGeneralTest(core.NewArrayAssignment("public", "fillable", rhs).String(), rhsOut),
+		api.NewGeneralTest(core.NewArrayAssignment("private", "fillable", rhs).String(), rhsOut2),
+		api.NewGeneralTest(core.NewArrayAssignment("", "fillable", rhs).String(), rhsOut3),
+	}
+
+	api.IterateAndTest(table, t)
+
+}
