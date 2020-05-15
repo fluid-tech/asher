4acd87e34eb22e173fb4190c2a46089ff984b42d
Rohan Nair <rrnair98@gmail.com>
2020-05-15 10:52:07 +0530

    wip

--- internal/models/column.go
+++ internal/models/column.go
@@ -9,7 +9,7 @@ type Column struct {
 	Validations        string   `json:"validations"`        // a set of validation rules separated by |
 	Index              bool     `json:"index"`              // should this col be indexed
 	Allowed            []string `json:"allowed"`            // a set of allowed values to be used in cases of enums
-	Invisible          bool     `json:"invisible"`          // indicates whether this col is presented when jsonified
+	Hidden             bool     `json:"hidden"`             // indicates whether this col is presented when jsonified
 	Guarded            bool     `json:"fillable"`           // indicates whether or not this field is fillable ie placed in the fillable array
 	Primary            bool     `json:"primary"`            // indicates whether or not this field is the pk for this table
 	Unique             bool     `json:"unique"`             // whether or not this col is unique
