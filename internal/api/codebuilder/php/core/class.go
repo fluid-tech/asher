667c4768253de5c737c463fe8cd0053b9ee22bb4
Rohan Nair <rrnair98@gmail.com>
2020-05-15 09:20:13 +0530

    adding default ctor method

--- internal/api/codebuilder/php/core/class.go
+++ internal/api/codebuilder/php/core/class.go
@@ -18,28 +18,89 @@ type Class struct {
 	Imports        []string
 }
 
-func (klass Class) Id() string {
+func NewClass() *Class {
+	return &Class{
+		Tabs:           0,
+		Name:           "",
+		Extends:        "",
+		ImplementsList: []string{},
+		Members:        []*TabbedUnit{},
+		Functions:      []*Function{},
+		Package:        "",
+		Imports:        []string{},
+	}
+}
+
+func (klass *Class) Id() string {
 	return klass.Name
 }
 
-func (klass Class) SetNumTabs(tabs int) {
+func (klass *Class) SetNumTabs(tabs int) {
 	klass.Tabs = tabs
 }
 
 func (klass Class) String() string {
 	var sb strings.Builder
+	klass.handlePackage(&sb)
+	klass.handleImports(&sb)
 	fmt.Fprint(&sb, TabbedString(uint(klass.Tabs)))
-	fmt.Fprint(&sb, "class ", klass.Name, " extends ", klass.Extends)
+	fmt.Fprint(&sb, "class ", klass.Name)
+	klass.handleExtends(&sb)
 	klass.handleImplementsList(&sb)
-	fmt.Fprint(&sb, " { \n")
+	fmt.Fprint(&sb, " {\n")
 	klass.handleMembers(&sb)
 	klass.handleFunctions(&sb)
-	fmt.Fprint(&sb, "\n\n", TabbedString(uint(klass.Tabs)), "}")
+	fmt.Fprint(&sb,  TabbedString(uint(klass.Tabs)), "}\n")
 
 	return sb.String()
 }
 
-func (klass Class) handleMembers(builder *strings.Builder) {
+/**
+Searches for the identifier in the members list
+*/
+func (klass *Class) FindInMembers(identifier string) (*TabbedUnit, error) {
+	for _, element := range klass.Members {
+		if (*element).Id() == identifier {
+			return element, nil
+		}
+	}
+	return nil, errors.New(fmt.Sprintf("couldnt find member with identifier %s", identifier))
+}
+
+/**
+Searches for the identifier in the functions list
+*/
+func (klass *Class) FindInFunctions(identifier string) (*Function, error) {
+	for _, element := range klass.Functions {
+		if element.Id() == identifier {
+			return element, nil
+		}
+	}
+	return nil, errors.New(fmt.Sprintf("couldnt find function with identifier %s", identifier))
+}
+
+func (klass *Class) handlePackage(builder *strings.Builder) {
+	if klass.Package != "" {
+		fmt.Fprint(builder, "namespace ", klass.Package, ";\n\n")
+	}
+}
+
+func (klass *Class) handleImports(builder *strings.Builder) {
+	if len(klass.Imports) > 0 {
+		for _, element := range klass.Imports {
+			fmt.Fprint(builder, "use ", element, ";\n")
+		}
+		fmt.Fprint(builder, "\n")
+	}
+}
+
+func (klass *Class) handleExtends(builder *strings.Builder) {
+	if klass.Extends != "" {
+		fmt.Fprint(builder, " extends ", klass.Extends)
+	}
+}
+
+func (klass *Class) handleMembers(builder *strings.Builder) {
 	if len(klass.Members) > 0 {
 		for _, element := range klass.Members {
 			(*element).SetNumTabs(klass.Tabs + 1)
@@ -51,7 +112,7 @@ func (klass Class) handleMembers(builder *strings.Builder) {
 	}
 }
 
-func (klass Class) handleImplementsList(builder *strings.Builder) {
+func (klass *Class) handleImplementsList(builder *strings.Builder) {
 	if len(klass.ImplementsList) > 0 {
 		_, err := fmt.Fprint(builder, " implements ", strings.Join(klass.ImplementsList[:], ", "))
 		if err != nil {
@@ -60,32 +121,11 @@ func (klass Class) handleImplementsList(builder *strings.Builder) {
 	}
 }
 
-func (klass Class) handleFunctions(builder *strings.Builder) {
+func (klass *Class) handleFunctions(builder *strings.Builder) {
 	if len(klass.Functions) > 0 {
 		for _, element := range klass.Functions {
 			element.SetNumTabs(klass.Tabs + 1)
-			_, err := fmt.Fprint(builder, element, "\n")
-			if err != nil {
-				fmt.Println("Error encounted ", err)
-			}
+			fmt.Fprint(builder, element, "\n")
 		}
 	}
-}
-
-func (klass *Class) FindInMembers(identifier string) (*TabbedUnit, error) {
-	for _, element := range klass.Members {
-		if (*element).Id() == identifier {
-			return element, nil
-		}
-	}
-	return nil, errors.New(fmt.Sprintf("couldnt find member with identifier %s", identifier))
-}
-
-func (klass *Class) FindInFunctions(identifier string) (*Function, error) {
-	for _, element := range klass.Functions {
-		if element.Id() == identifier {
-			return element, nil
-		}
-	}
-	return nil, errors.New(fmt.Sprintf("couldnt find function with identifier %s", identifier))
 }
