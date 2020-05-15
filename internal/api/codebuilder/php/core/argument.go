da9ce06b695f263e9117802407b25cb2ff7b4380
Gaurav Punjabi <gaurav.bpunjabi@gmail.com>
2020-05-15 23:18:59 +0530

    Update: removed tabbedString from String()

--- internal/api/codebuilder/php/core/argument.go
+++ internal/api/codebuilder/php/core/argument.go
@@ -1,10 +1,5 @@
 package core
 
-import (
-	"fmt"
-	"strings"
-)
-
 
 type Argument struct{
 	TabbedUnit
@@ -27,7 +22,5 @@ func (a *Argument) Id() string {
 }
 
 func (a *Argument) String() string {
-	var builder strings.Builder
-	fmt.Fprint(&builder, TabbedString(uint(a.tabs)), a.value)
-	return builder.String()
+	return a.value
 }
