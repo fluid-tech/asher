7bd8521639836c4df83327c22e9b393200d14f36
Gaurav Punjabi <gaurav.bpunjabi@gmail.com>
2020-05-16 00:19:59 +0530

    Added: SetName() and AddFillable()

--- internal/impl/laravel/5.8/handler/generator/ModelGenerator.go
+++ internal/impl/laravel/5.8/handler/generator/ModelGenerator.go
@@ -1,16 +1,46 @@
 package generator
 
-import "asher/internal/api/codebuilder/php/core"
+import (
+	"asher/internal/api/codebuilder/php/builder"
+	"asher/internal/api/codebuilder/php/builder/interfaces"
+	"github.com/iancoleman/strcase"
+)
 
 type ModelGenerator struct {
-	class *core.Class
+	class interfaces.Class
+	fillables []string
 }
 
 /**
-  Creates a new instance of this builder with a new core.Class
+ Creates a new instance of this builder with a new core.Class
  */
 func NewModelGenerator() *ModelGenerator {
 	return &ModelGenerator{
-		class: core.NewClass(),
+		class: builder.NewClassBuilder(),
 	}
+}
+
+/**
+ Set's the name of the model class.
+ Parameters:
+	- tableName: the name of table specified in asher config.
+ Returns:
+	- instance of the generator object
+ */
+func (modelGenerator *ModelGenerator) SetName(tableName string) *ModelGenerator {
+	className := strcase.ToCamel(tableName)
+	modelGenerator.SetName(className)
+	return modelGenerator
+}
+
+/**
+ Adds the given column to fillable fields of this model
+ Parameters:
+	- columnName: the name of the column to add
+ Returns:
+	- instance of the generator object
+ */
+func (modelGenerator *ModelGenerator) AddFillable(columnName string) *ModelGenerator {
+	modelGenerator.fillables = append(modelGenerator.fillables, columnName)
+	return modelGenerator
 }
