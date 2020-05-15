cc5b609f1fd9c39bd8b3fbd9be3228a8105e5f70
Rohan Nair <rrnair98@gmail.com>
2020-05-15 12:29:52 +0530

    adding comments

--- internal/api/emitter_file.go
+++ internal/api/emitter_file.go
@@ -6,7 +6,7 @@ type EmitterFile struct {
 	FileName string      // name of the file
 	Path     string      // path to store it in
 	klass    *core.Class // class that must be stringified
-	FileType int         // 0 - migration, 1 - model, 2- mutator, 3-transactor
+	FileType int         // 0 - migration, 1 - model, 2- mutator, 3-transactor, 5 - controller, 6- routeFile
 }
 
 func NewEmitterFile(fileName string, path string, class *core.Class, fileType int) *EmitterFile {
