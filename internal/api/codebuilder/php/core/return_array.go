23fa0c55a9bef9be0be863683722b516e54c0806
Pranay Patro <pranaypatro11@gmail.com>
2020-05-16 22:28:30 +0530

    Added method to convertMapToStringAssociativeArray in ReturnArray struct

--- internal/api/codebuilder/php/core/return_array.go
+++ internal/api/codebuilder/php/core/return_array.go
@@ -11,6 +11,20 @@ type ReturnArray struct{
 	Statements []string
 }
 
+func convertMapToStringAssociativeArray(rulesMap map[string]string ) []string {
+	var returnVal []string
+	for colName, colRule := range rulesMap {
+		returnVal = append(returnVal, colName + " => \"" + colRule + "\"")
+	}
+	return returnVal
+}
+
+func NewReturnArrayFromMap(arr map[string]string) *ReturnArray {
+	return &ReturnArray{
+		Statements: convertMapToStringAssociativeArray(arr),
+	}
+}
+
 func NewReturnArray(arr []string) *ReturnArray {
 	return &ReturnArray{
 		Statements: arr,
