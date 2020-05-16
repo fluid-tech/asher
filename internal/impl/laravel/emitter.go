32ea8917fc8e4b275288295b08a5e67d74751c4c
Dhano <dhananjay62.dg@gmail.com>
2020-05-16 14:04:35 +0530

    local changes

--- internal/impl/laravel/emitter.go
+++ internal/impl/laravel/emitter.go
@@ -29,7 +29,6 @@ func (e Emitter) Emit(value interface{}) {
 	//GetFromRegistry("softDeletes").Handle(model.Name, model.SoftDeletes)
 	//GetFromRegistry("timestamps").Handle(model.Name, model.Timestamps)
 	GetFromRegistry("auditCols").Handle(model.Name, model.AuditCols)
-	GetFromRegistry("controller").handle("Order", model.Controller)
 
 
 }
