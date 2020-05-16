cc95f39615dfe00148e1bd3244ee6d55709ab508
Rohan Nair <rrnair98@gmail.com>
2020-05-16 16:25:01 +0530

    emitter file is now an interface

--- internal/api/emitter_file.go
+++ internal/api/emitter_file.go
@@ -1,19 +1,9 @@
 package api
 
-import "asher/internal/api/codebuilder/php/core"
-
-type EmitterFile struct {
-	FileName string      // name of the file
-	Path     string      // path to store it in
-	Klass    *core.Class // class that must be stringified
-	FileType int         // 0 - migration, 1 - model, 2- mutator, 3-transactor, 5 - controller, 6- routeFile
-}
-
-func NewEmitterFile(fileName string, path string, class *core.Class, fileType int) *EmitterFile {
-	return &EmitterFile{
-		FileName: fileName,
-		Path:     path,
-		Klass:    class,
-		FileType: fileType,
-	}
+type EmitterFile interface {
+	FileName() string       // name of the file
+	Path() string           // path to store it in
+	Content() []*TabbedUnit // The contents of the file
+	FileType() int          // 0 - migration, 1 - model, 2- mutator, 3-transactor, 5 - controller, 6- routeFile
+	String() string
 }
