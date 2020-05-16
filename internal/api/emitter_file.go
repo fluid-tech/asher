7d0627a61c8c9cf6917858757b4e86e0f3a579a8
Rohan Nair <rrnair98@gmail.com>
2020-05-16 11:36:07 +0530

    making Klass public

--- internal/api/emitter_file.go
+++ internal/api/emitter_file.go
@@ -5,7 +5,7 @@ import "asher/internal/api/codebuilder/php/core"
 type EmitterFile struct {
 	FileName string      // name of the file
 	Path     string      // path to store it in
-	klass    *core.Class // class that must be stringified
+	Klass    *core.Class // class that must be stringified
 	FileType int         // 0 - migration, 1 - model, 2- mutator, 3-transactor, 5 - controller, 6- routeFile
 }
 
@@ -13,7 +13,7 @@ func NewEmitterFile(fileName string, path string, class *core.Class, fileType in
 	return &EmitterFile{
 		FileName: fileName,
 		Path:     path,
-		klass:    class,
+		Klass:    class,
 		FileType: fileType,
 	}
 }
