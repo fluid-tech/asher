9cb2db483aa08bbdb164373cb1da2ccaa90253d3
Gaurav Punjabi <gaurav.bpunjabi@gmail.com>
2020-05-17 16:26:05 +0530

    Updated: removed string from emitter

--- internal/api/emitter_file.go
+++ internal/api/emitter_file.go
@@ -5,5 +5,4 @@ type EmitterFile interface {
 	Path() string           // path to store it in
 	Content() []*TabbedUnit // The contents of the file
 	FileType() int          // 0 - migration, 1 - model, 2- mutator, 3-transactor, 5 - controller, 6- routeFile
-	String() string
 }
