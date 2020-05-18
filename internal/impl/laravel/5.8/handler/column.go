e5450641278951cae12ccffd52a31392cc130138
Pranay Patro <pranaypatro11@gmail.com>
2020-05-18 11:56:04 +0530

    Emitted PhpFileEmitter in column Handler

--- internal/impl/laravel/5.8/handler/column.go
+++ internal/impl/laravel/5.8/handler/column.go
@@ -39,7 +39,7 @@ func (columnHandler *ColumnHandler) handleModel(modelName string, colArr []model
 	//fmt.Print(modelGenerator.Build().String())
 	context.GetFromRegistry("model").AddToCtx(modelName, modelGenerator)
 	modelGeneratorRef := api.Generator(modelGenerator)
-	phpEmitter := core.NewPhpEmitterFile(modelName, api.MODEL_PATH, &modelGeneratorRef, api.MODEL)
+	phpEmitter := core.NewPhpEmitterFile(modelName, api.ModelPath, &modelGeneratorRef, api.MODEL)
 	return phpEmitter
 }
 
@@ -57,7 +57,7 @@ func (columnHandler *ColumnHandler) handleMigration(identifier string, columnArr
 	fmt.Println(klass)
 	context.GetFromRegistry("migration").AddToCtx(identifier, migrationGenerator)
 	modelGeneratorRef := api.Generator(migrationGenerator)
-	phpEmitter := core.NewPhpEmitterFile(identifier, api.MODEL_PATH, &modelGeneratorRef, api.MODEL)
+	phpEmitter := core.NewPhpEmitterFile(identifier, api.ModelPath, &modelGeneratorRef, api.MODEL)
 	return phpEmitter
 }
 
