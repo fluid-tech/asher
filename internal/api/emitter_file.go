ddb414411fb75cd8fa233a9f2a4c7745c8bf2523
rrnair-98 <30407158+rrnair-98@users.noreply.github.com>
2020-05-19 15:35:31 +0530

    fmt

--- internal/api/emitter_file.go
+++ internal/api/emitter_file.go
@@ -3,7 +3,7 @@ package api
 type EmitterFile interface {
 	FileName() 	string      // name of the file
 	Path() 		string      // path to store it in
-	Generator() *Generator  // The contents of the file
+	Generator() 	*Generator  // The contents of the file
 	FileType() 	int         // 0 - migration, 1 - model, 2- mutator, 3-transactor, 5 - controller, 6- routeFile
 }
 
