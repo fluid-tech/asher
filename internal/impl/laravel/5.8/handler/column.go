0c971fa77e9a7302a4876a1af3bb2b04a25cf640
Pranay Patro <pranaypatro11@gmail.com>
2020-05-19 13:55:44 +0530

    moved columndatatype processor to helper for better management

--- internal/impl/laravel/5.8/handler/column.go
+++ internal/impl/laravel/5.8/handler/column.go
@@ -5,6 +5,7 @@ import (
 	"asher/internal/api/codebuilder/php/core"
 	"asher/internal/impl/laravel/5.8/handler/context"
 	"asher/internal/impl/laravel/5.8/handler/generator"
+	"asher/internal/impl/laravel/5.8/handler/helper"
 	"asher/internal/models"
 	"fmt"
 	"strings"
@@ -44,12 +45,13 @@ func (columnHandler *ColumnHandler) handleModel(modelName string, colArr []model
 }
 
 func (columnHandler *ColumnHandler) handleMigration(identifier string, columnArr []models.Column) api.EmitterFile {
-	var statementsArr []core.SimpleStatement
+	var statementsArr []*core.SimpleStatement
 	for _, singleColumn := range columnArr {
 		statementsArr = append(
 			statementsArr,
 			columnHandler.handleSingleColumn(identifier, singleColumn),
 		)
+
 	}
 
 	migrationGenerator := generator.NewMigrationGenerator().SetName(identifier).AddColumns(statementsArr)
@@ -68,14 +70,14 @@ func (columnHandler *ColumnHandler) handleValidation(modelGenerator *generator.M
 	}
 }
 
-func (columnHandler *ColumnHandler) handleSingleColumn(modelName string, column models.Column) core.SimpleStatement {
+func (columnHandler *ColumnHandler) handleSingleColumn(modelName string, column models.Column) *core.SimpleStatement {
 
 	if column.Primary {
 		//Handle PrimaryKey
 		return columnHandler.handlePrimary(column.ColType, column.Name, column.GenerationStrategy)
 	} else if column.ColType == "reference" {
 		// Handle ForeignKey
-		return columnHandler.handleForeign(column.Name, column.Table, column.OnDelete)
+		return columnHandler.handleForeign(column.Name, column.Table, column.OnDelete, column.Nullable)
 	} else {
 		// Handle Other Columns
 		return columnHandler.handleOther(column)
@@ -95,7 +97,7 @@ func (columnHandler *ColumnHandler) handleGuarded(modelGenerator *generator.Mode
 	}
 }
 
-func (columnHandler *ColumnHandler) handlePrimary(colType string, colName string, genStrat string) core.SimpleStatement {
+func (columnHandler *ColumnHandler) handlePrimary(colType string, colName string, genStrat string) *core.SimpleStatement {
 	var generatedLine string
 	if genStrat == "auto_increment" {
 		primaryKeyMethodName := columnHandler.primaryKeyMethodNameGenerator(colType)
@@ -106,13 +108,13 @@ func (columnHandler *ColumnHandler) handlePrimary(colType string, colName string
 	} else {
 		panic("input Type does not match with the defined keywords (uuid, auto_increment)")
 	}
-	return core.SimpleStatement{
+	return &core.SimpleStatement{
 		SimpleStatement: generatedLine,
 	}
 
 }
 
-func (columnHandler *ColumnHandler) handleOther(column models.Column) core.SimpleStatement {
+func (columnHandler *ColumnHandler) handleOther(column models.Column) *core.SimpleStatement {
 	var generatedLine string
 	colTypeVal := columnHandler.ColTypeSwitcher(column.ColType, column.Name, column.Allowed)
 	defaultVal := columnHandler.handleDefaultValue(column.DefaultVal)
@@ -120,21 +122,21 @@ func (columnHandler *ColumnHandler) handleOther(column models.Column) core.Simpl
 	uniqueVal := columnHandler.handleUnique(column.Unique)
 	indexVal := columnHandler.handleIndex(column.Index)
 	generatedLine = fmt.Sprintf("$table->%s%s%s%s%s", colTypeVal, defaultVal, nullableVal, uniqueVal, indexVal)
-	return core.SimpleStatement{
+	return &core.SimpleStatement{
 		SimpleStatement: generatedLine,
 	}
 }
 
-func (columnHandler *ColumnHandler) handleForeign(colName string, colTable string, colOnDelete string) core.SimpleStatement {
+func (columnHandler *ColumnHandler) handleForeign(colName string, colTable string, colOnDelete string, isNullable bool) *core.SimpleStatement {
 	//$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
 	var sb string
 	var splitedArr []string
 	splitedArr = strings.Split(colTable, ":")
-	sb = fmt.Sprintf("$table->foreign('%s')->references('%s')->on('%s')->onDelete('%s')", colName, splitedArr[1], splitedArr[0], colOnDelete)
+	sb = fmt.Sprintf("$table->foreign('%s')->references('%s')->on('%s')->onDelete('%s')%s", colName, splitedArr[1], splitedArr[0], colOnDelete, columnHandler.handleNullable(isNullable))
 
 	// TODO: check if the onDelete Value is sanitized of not
 	// TODO: do more research on cascade ondelete will it be mandatory or not
-	return core.SimpleStatement{
+	return &core.SimpleStatement{
 		SimpleStatement: sb,
 	}
 }
@@ -171,11 +173,11 @@ func (columnHandler *ColumnHandler) primaryKeyMethodNameGenerator(colType string
 	switch colType {
 	case "integer":
 		return "increments"
-	case "mediumInt":
+	case "mediumInteger":
 		return "mediumIncrements"
 	case "smallInteger":
 		return "smallIncrements"
-	case "tinyInt":
+	case "tinyInteger":
 		return "tinyIncrements"
 	case "bigInteger":
 		return "bigIncrements"
@@ -184,29 +186,47 @@ func (columnHandler *ColumnHandler) primaryKeyMethodNameGenerator(colType string
 	}
 }
 
-func (columnHandler *ColumnHandler) handleAllowedKeywordsToString(allowed []string) string {
-	bldr := "'" + strings.Join(allowed, "', '") + "'"
-	return "[" + bldr + "]"
-}
-
 /*
 	This method will have all the keys defined by asher as valid input value and return
 	its respective laravel method name
 */
 func (columnHandler *ColumnHandler) ColTypeSwitcher(colType string, colName string, allowed []string) string {
-	var columnNameBracket = "('" + colName + "')"
 	switch colType {
 	// TODO : Add more column types here
 	case "unsignedBigInteger":
-		return "unsignedBigInteger" + columnNameBracket
+		return helper.UnsignedBigInteger(colName)
 	case "bigInteger":
-		return "bigInteger" + columnNameBracket
+		return helper.BigInteger(colName)
+	case "unsignedInteger":
+		return helper.UnsignedInteger(colName)
+	case "integer":
+		return helper.Integer(colName)
+	case "unsignedTinyInteger":
+		return helper.UnsignedTinyInteger(colName)
+	case "tinyInteger":
+		return helper.TinyInteger(colName)
+	case "unsignedMediumInteger":
+		return helper.UnsignedMediumInteger(colName)
+	case "mediumInteger":
+		return helper.MediumInteger(colName)
 	case "string":
-		return "string" + columnNameBracket
+		return helper.String(colName)
+	case "boolean":
+		return helper.Boolean(colName)
+	case "char":
+		return helper.Char(colName)
+	case "date":
+		return helper.Date(colName)
+	case "double":
+		return helper.Double(colName)
+	case "float":
+		return helper.Float(colName)
 	case "enum":
-		return "enum" + "('" + colName + "', " + columnHandler.handleAllowedKeywordsToString(allowed) + ")"
+		return helper.Enum(colName, allowed)
+	case "set":
+		return helper.Set(colName, allowed)
 	default:
 		// TODO: Log this error and replace it with formatted error message.
-		panic("not supported or wrong input in ColTypeSwitcher" + colType)
+		panic("not supported or wrong input in ColTypeSwitcher :- " + colType)
 	}
 }
