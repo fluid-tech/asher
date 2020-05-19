957b40cc0364ce9ffa68a037d78b64c5d00185a8
Rohan Nair <rrnair98@gmail.com>
2020-05-19 14:52:53 +0530

    change: formatting file

--- internal/api/emitter_file.go
+++ internal/api/emitter_file.go
@@ -1,10 +1,10 @@
 package api
 
 type EmitterFile interface {
-	FileName() string      // name of the file
-	Path() string          // path to store it in
-	Generator() *Generator // The contents of the file
-	FileType() int         // 0 - migration, 1 - model, 2- mutator, 3-transactor, 5 - controller, 6- routeFile
+	FileName() 	string      // name of the file
+	Path() 		string      // path to store it in
+	Generator() *Generator  // The contents of the file
+	FileType() 	int         // 0 - migration, 1 - model, 2- mutator, 3-transactor, 5 - controller, 6- routeFile
 }
 
 const (
@@ -14,7 +14,7 @@ const (
 	Transactor = 3
 
 	Controller = 5
-	RouterFile  = 6
+	RouterFile = 6
 )
 
 const (
