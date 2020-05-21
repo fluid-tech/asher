abef563f29cf78c378b4efe63fc6da1828da60ca
karunakukreja <karunakukreja06@gmail.com>
2020-05-21 23:39:04 +0530

    Added mutator transactor controller emitter

--- internal/impl/laravel/5.8/handler/controller.go
+++ internal/impl/laravel/5.8/handler/controller.go
@@ -6,7 +6,7 @@ import (
 	"asher/internal/impl/laravel/5.8/handler/context"
 	"asher/internal/impl/laravel/5.8/handler/generator"
 	"asher/internal/models"
-	"fmt"
+	"strings"
 )
 
 type ControllerHandler struct {
@@ -21,51 +21,80 @@ func (controllerHandler *ControllerHandler) Handle(
 	identifier string, value interface{}) ([]*api.EmitterFile, error) {
 
 	tempValue := value.(models.Controller)
+	fileToEmit := []*api.EmitterFile{}
 	if tempValue.Rest {
-		controllerHandler.HandleRestController(identifier, value)
+		fileToEmit = append(fileToEmit,controllerHandler.HandleRestController(identifier, value)...)
 	}
+	fileToEmit = append(fileToEmit, controllerHandler.HandleMutator(identifier)...)
+	fileToEmit = append(fileToEmit, controllerHandler.HandleTransactor(identifier)...)
+	fileToEmit = append(fileToEmit, controllerHandler.HandleRestController(identifier,value)...)
 
-	var emitFiles []*api.EmitterFile
+
 	routeFiles, _ := controllerHandler.HandleRoutes(identifier, value)
-	emitFiles = append(emitFiles, routeFiles...)
-	return emitFiles, nil
+	fileToEmit = append(fileToEmit, routeFiles...)
+	return fileToEmit, nil
 }
 
 func (controllerHandler *ControllerHandler) HandleRestController(
-	identifier string, value interface{}) (api.EmitterFile, error) {
+	identifier string, value interface{}) ([]*api.EmitterFile) {
+
+	fileToEmiit := []*api.EmitterFile{}
 	controller := value.(models.Controller)
 
-	controllerGenerator := generator.NewControllerGenerator(controller)
-	controllerGenerator.SetIdentifier(identifier)
-	context.GetFromRegistry("controller").AddToCtx(identifier+`RestController`, controllerGenerator)
-	tempRouteEmitter := core.NewPhpEmitterFile("", "", nil, 1)
-	return tempRouteEmitter, nil
+	conGen := generator.NewControllerGenerator()
+	conGen.SetIdentifier(identifier)
+	if controller.HttpMethods != nil {
+		if len(controller.HttpMethods) >= 0 {
+			conGen.AddConstructorFunction()
+			for _, element := range controller.HttpMethods {
+				switch strings.ToLower(element) {
+				case "post":
+					conGen.AddCreateFunction()
+				case "get":
+					conGen.AddFindByIdFunction().AddGetAllFunction()
+				case "put":
+					conGen.AddUpdateFunction()
+				case "delete":
+					conGen.AddDeleteFunction()
+				}
+			}
+		}
+	} else {
+		conGen.AddAllFunctionsInController()
+	}
+	context.GetFromRegistry("controller").AddToCtx(identifier+`RestController`, conGen)
+	conGenRef := api.Generator(conGen)
+	tempRouteEmitter := api.EmitterFile(
+		core.NewPhpEmitterFile(identifier+"RestController",  api.ControllerPath, &conGenRef, api.Controller))
+	fileToEmiit = append(fileToEmiit, &tempRouteEmitter)
+	return fileToEmiit
 }
 
-func (controllerHandler *ControllerHandler) HandleTransactor(
-	identifier string, value interface{}) ([]api.EmitterFile, error) {
-	fileToEmitt := []api.EmitterFile{}
-	transactorGenerator := generator.NewTransactorGenerator()
-	transactorGenerator.SetIdentifier(identifier)
+func (controllerHandler *ControllerHandler) HandleTransactor(identifier string) ([]*api.EmitterFile) {
+	filesToEmmit := []*api.EmitterFile{}
+	transactorGen := generator.NewTransactorGenerator()
+	transactorGen.SetIdentifier(identifier)
 
-	context.GetFromRegistry("controller").AddToCtx(identifier+"Transactor", transactorGenerator)
-	tempRouteEmitter := core.NewPhpEmitterFile("", "", nil, 1)
+	context.GetFromRegistry("controller").AddToCtx(identifier+"Transactor", transactorGen)
+	transactorGenRef := api.Generator(transactorGen)
+	tempRouteEmitter := api.EmitterFile(core.NewPhpEmitterFile(identifier+"Transactor",  api.TransactorPath, &transactorGenRef, api.Transactor))
 
-	fileToEmitt = append(fileToEmitt, tempRouteEmitter)
-	return fileToEmitt, nil
+	filesToEmmit = append(filesToEmmit, &tempRouteEmitter)
+	return filesToEmmit
 }
 
-func (controllerHandler *ControllerHandler) HandleMutator(
-	identifier string, value interface{}) ([]api.EmitterFile, error) {
-	fileToEmitt := []api.EmitterFile{}
-	mutatorGenerator := generator.NewMutatorGenerator()
-	mutatorGenerator.SetIdentifier(identifier)
+func (controllerHandler *ControllerHandler) HandleMutator(identifier string) ([]*api.EmitterFile) {
+	filesToEmit := []*api.EmitterFile{}
+	mutatorGen := generator.NewMutatorGenerator()
+	mutatorGen.SetIdentifier(identifier)
 
-	context.GetFromRegistry("controller").AddToCtx(identifier+"Mutator", mutatorGenerator)
-	tempRouteEmitter := core.NewPhpEmitterFile("", "", nil, 1)
+	context.GetFromRegistry("controller").AddToCtx(identifier+"Mutator", mutatorGen)
+	mutatorGenRef := api.Generator(mutatorGen)
+	tempRouteEmitter := api.EmitterFile(
+		core.NewPhpEmitterFile(identifier+"Mutator",  api.MutatorPath, &mutatorGenRef, api.Mutator))
 
-	fileToEmitt = append(fileToEmitt, tempRouteEmitter)
-	return fileToEmitt, nil
+	filesToEmit = append(filesToEmit, &tempRouteEmitter)
+	return filesToEmit
 }
 
 func (controllerHandler *ControllerHandler) HandleQuery(
