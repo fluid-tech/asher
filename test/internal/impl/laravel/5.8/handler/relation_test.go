d01de3709eb339f7bdec0416a69ac61dfe2c87ab
Pranay Patro <pranaypatro11@gmail.com>
2020-05-22 13:34:39 +0530

    Added Testcase for Relation

---
+++ test/internal/impl/laravel/5.8/handler/relation_test.go
@@ -0,0 +1,22 @@
+package handler
+
+import (
+	"asher/internal/impl/laravel/5.8/handler"
+	"asher/internal/impl/laravel/5.8/handler/context"
+	"asher/internal/impl/laravel/5.8/handler/generator"
+	"asher/internal/models"
+	"testing"
+)
+
+func Test_Relations(t *testing.T) {
+	modelGen := generator.NewModelGenerator().SetName("Orders")
+	context.GetFromRegistry("model").AddToCtx("Orders", modelGen)
+	handler.NewRelationshipHandler().Handle("Orders", models.Relation{
+		HasMany: []string{ "OrderProducts:order_id,pk_col", "OrderProducts", "OrderProducts:order_id" },
+		HasOne:  []string{ "OrderProducts:order_id,pk_col", "OrderProducts", "OrderProducts:order_id" },
+	})
+	stringOp := modelGen.String()
+	if stringOp != output_1 {
+		t.Error("Test Failed Expected '" + output_1 + "' But Found '" + stringOp + "'" )
+	}
+}
---
+++ test/internal/impl/laravel/5.8/handler/relation_test_input.go
@@ -0,0 +1,39 @@
+package handler
+
+const output_1 = `namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Orders extends Model {
+     function OrderProducts() {
+        return $this->hasMany('App\OrderProducts','order_id','pk_col');
+    }
+
+
+     function OrderProducts() {
+        return $this->hasMany('App\OrderProducts','orders_id','id');
+    }
+
+
+     function OrderProducts() {
+        return $this->hasMany('App\OrderProducts','order_id','id');
+    }
+
+
+     function OrderProducts() {
+        return $this->hasOne('App\OrderProducts','order_id','pk_col');
+    }
+
+
+     function OrderProducts() {
+        return $this->hasOne('App\OrderProducts','orders_id','id');
+    }
+
+
+     function OrderProducts() {
+        return $this->hasOne('App\OrderProducts','order_id','id');
+    }
+
+
+}
+`
